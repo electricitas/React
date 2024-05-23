b3dbd863d4cb2422572646ac6399cdb2dc39a751
electricitas <142588614+electricitas@users.noreply.github.com>
2024-05-23 15:08:51 +0300

    Add files via upload
    Kissoja

---
+++ Kissa/components/Kisut.jsx
@@ -0,0 +1,66 @@
+import "../src/style.css";
+import kissa1 from "../images/whiskerson.png";
+import kissa2 from "../images/fluffy.png";
+import kissa3 from "../images/felix.png";
+import kissa4 from "../images/pumpkin.png";
+
+function Kisut() {
+  return (
+    <div className="contacts">
+      <div className="contact-card">
+        <img src={kissa1} />
+        <h3>Mr. Whiskerson</h3>
+        <div className="info-group">
+          <img src="../images/phone-icon.png" />
+          <p>(212) 555-1234</p>
+        </div>
+        <div className="info-group">
+          <img src="./images/mail-icon.png" />
+          <p>mr.whiskaz@catnap.meow</p>
+        </div>
+      </div>
+
+      <div className="contact-card">
+        <img src={kissa2} />
+        <h3>Fluffykins</h3>
+        <div className="info-group">
+          <img src="../images/phone-icon.png" />
+          <p>(212) 555-2345</p>
+        </div>
+        <div className="info-group">
+          <img src="./images/mail-icon.png" />
+          <p>fluff@me.com</p>
+        </div>
+      </div>
+
+      <div className="contact-card">
+        <img src={kissa3} />
+        <h3>Felix</h3>
+        <div className="info-group">
+          <img src="./images/phone-icon.png" />
+          <p>(212) 555-4567</p>
+        </div>
+        <div className="info-group">
+          <img src="./images/mail-icon.png" />
+          <p>thecat@hotmail.com</p>
+        </div>
+      </div>
+
+      <div className="contact-card">
+        <img src={kissa4} />
+        <h3>Pumpkin</h3>
+        <div className="info-group">
+          <img src="./images/phone-icon.png" />
+          <p>(0800) CAT KING</p>
+        </div>
+        <div className="info-group">
+          <img src="./images/mail-icon.png" />
+          <p>pumpkin@scrimba.com</p>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Kisut;
