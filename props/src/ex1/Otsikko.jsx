c14f92fc365a0a8562267c708e179bed2c00178c
electricitas <142588614+electricitas@users.noreply.github.com>
2024-05-23 13:59:30 +0300

    Add files via upload
    Props

---
+++ props/src/ex1/Otsikko.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Otsikko = () => {
+  return (
+<div>
+  <h1>Tämä on otsikko</h1>
+  </div>
+  );
+};
+
+export default Otsikko;
---
+++ props/src/ex1/Section.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Kappale from './Kappale';
+import Otsikko from './Otsikko';
+
+
+const Section = () => {
+  return (
+    <div>
+      <Otsikko />
+      <Kappale teksti="Tämä on ensimmäisen tehtävän teksti" />
+    </div>
+  );
+};
+
+export default Section;
